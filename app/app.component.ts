70bfb8787bdbabb8598ee22c9a34da4145c16b83
Yohanan Assefa <nanahoy@gmail.com>
2017-10-11 13:48:21 -0600

    Create separate homepage component

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Componen({
+  moduleID: module.id,
+  selector: 'app',
+  template: '<h1>Navigation</h1>'
+})
+export class AppComponent {
+  title: 'Freelance Bootcamp Dashboard';
+}
